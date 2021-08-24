31f1e6ffaca914ea963308d50bc66ffca04b4b35
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-08-24 17:31:16 +0530

    Added css styles to contact us component

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -4,13 +4,15 @@ export const ContactUs = () => {
   return (
     <section className="contact-us">
       <div className="container contact-us__content">
-        <div className="section__header ">
+        <div className="contact-us__header ">
           <h2 className="section__title contact-us__main-title mx-auto">
             Ready to get started?
           </h2>
         </div>
-        <div className="section__body ">
-          <a href="google.com">contact us</a>
+        <div className="contact-us__body ">
+          <a href="google.com" className="contact__cta">
+            contact us
+          </a>
         </div>
       </div>
     </section>
