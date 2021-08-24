1f8f1052ea53ee189c4bb386350d8a12b5377ab4
Manoj Jayapprakash <manoj.jayapprakash@gmail.com>
2021-08-24 17:14:20 +0530

    Added contact us component

---
+++ src/components/ContactUs.js
@@ -0,0 +1,18 @@
+import './ContactUs.css';
+
+export const ContactUs = () => {
+  return (
+    <section className="contact-us">
+      <div className="container contact-us__content">
+        <div className="section__header ">
+          <h2 className="section__title contact-us__main-title mx-auto">
+            Ready to get started?
+          </h2>
+        </div>
+        <div className="section__body ">
+          <a href="google.com">contact us</a>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { Hero } from '../components/Hero';
 import { Features } from '../components/Features';
 import { Testimonials } from '../components/Testimonials';
+import { ContactUs } from '../components/ContactUs';
 
 export const Home = () => {
   return (
@@ -8,6 +9,7 @@ export const Home = () => {
       <Hero />
       <Features />
       <Testimonials />
+      <ContactUs />
     </>
   );
 };
